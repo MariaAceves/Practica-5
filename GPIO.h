faf226b42924fe0c438721179bb9dd2b6d849429
MariaAceves <84421662+MariaAceves@users.noreply.github.com>
2021-07-28 20:24:35 -0500

    Add files via upload

---
+++ GPIO.h
@@ -0,0 +1,24 @@
+/*
+ * GPIO.h
+ *
+ *  Created on: 13 jul. 2021
+ *
+ */
+
+#ifndef GPIO_GPIO_H_
+#define GPIO_GPIO_H_
+
+#include "Prototype.h"
+
+#include "GPIO_priv.h"
+
+#define OFF				(0U)
+#define OUTPUT			(0U)
+#define INPUT			(1U)
+#define ON				(1U)
+
+#define pinMode(pin, dir)			GPIO_vfnpinMode((uint8*)#pin, dir, sizeof(#pin) - 1)
+#define digitalWrite(pin, state)	GPIO_vfndigitalWrite((uint8*)#pin, state, sizeof(#pin) - 1)
+
+
+#endif /* GPIO_GPIO_H_ */
---
+++ GPIO_vArduino.c
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2016-2018 NXP
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * o Redistributions of source code must retain the above copyright notice, this list
+ *   of conditions and the following disclaimer.
+ *
+ * o Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ *
+ * o Neither the name of NXP Semiconductor, Inc. nor the names of its
+ *   contributors may be used to endorse or promote products derived from this
+ *   software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+ 
+/**
+ * @file    GPIO_vArduino.c
+ * @brief   Application entry point.
+ */
+#include <stdio.h>
+#include "GPIO.h"
+/* TODO: insert other include files here. */
+
+/* TODO: insert other definitions and declarations here. */
+
+/*
+ * @brief   Application entry point.
+ *
+ */
+void Delay_led (void);
+unsigned long x=1000000;
+
+int main(void) {
+
+
+    /* Force the counter to be placed into memory. */
+	pinMode(B18, OUTPUT);
+	pinMode(B19, OUTPUT);
+	pinMode(D1, OUTPUT);
+
+	digitalWrite(B18, ON);
+	digitalWrite(D1, ON);
+	digitalWrite(B19, ON);
+	/* Enter an infinite loop, just incrementing a counter. */
+    while(1) {
+    	Delay_led();
+    	digitalWrite(B18, ON);
+
+    	digitalWrite(B18, OFF);
+    	Delay_led();
+
+    	digitalWrite(D1,ON);
+    	Delay_led();
+
+    	digitalWrite(D1, OFF);
+    	Delay_led();
+
+    	digitalWrite(B19, ON);
+    	Delay_led();
+
+    	digitalWrite(B19, OFF);
+    	Delay_led();
+
+    }
+    return 0 ;
+}
+
+void Delay_led (void){
+	while(x){
+		x--;
+	}
+	x=2000000;
+}
+
